e618ee2f27043e1b1e401b37d64c7e44f98ad0bb
Alexisreau97 <alexis.reaupro@gmail.com>
2018-10-29 15:37:14 +0100

    add home page

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,5 @@
+class IndexController < ApplicationController
+	def index
+	  @all_gossips = Gossip.all
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'index#index'
   get '/team', to: 'team#team'
   get '/contact', to: 'contact#contact'
   get 'welcome/:first_name_id', to: 'welcome#first_name'
